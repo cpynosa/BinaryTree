3aa6341d2a9a67c46083bd5cf92f50d45bb7c9a9
Christopher  Espinosa <ignaciorazo77710@gmail.com>
2025-08-19 10:22:17 -0600

    Agregar archivos de proyecto.

---
+++ BinaryTree.cpp
@@ -0,0 +1,12 @@
+// BinaryTree.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
